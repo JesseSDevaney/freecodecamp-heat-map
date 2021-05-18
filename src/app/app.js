1ac13df26712591f6f3eb936fe4d1a6255cba861
Jesse Devaney <22608765+JesseSDevaney@users.noreply.github.com>
2021-05-18 14:04:16 -0400

    Create x-axis and x-axis label
    - Convert year strings to numbers in data
    - Create linear scale for x-axis
    - Create x-axis and remove commas from tick format
    - Plot x-axis
    - Plot x-label and set font-size for it

--- src/app/app.js
+++ src/app/app.js
@@ -1,12 +1,41 @@
 import * as d3 from "d3";
 import "./app.scss";
 
-export default function createHeatMap() {
+export default function createHeatMap(data) {
   const width = 800;
   const height = 600;
 
-  d3.select("#root")
+  const svg = d3
+    .select("#root")
     .append("svg")
     .attr("id", "chart")
     .attr("viewBox", `0 0 ${width} ${height}`);
+
+  const padWidth = 0.1 * width;
+  const padHeight = 0.1 * height;
+
+  // x-axis
+  const years = data["monthlyVariance"].map(({ year }) => parseInt(year, 10));
+  const xScale = d3
+    .scaleLinear()
+    .domain([d3.min(years) - 0.5, d3.max(years) + 0.5])
+    .range([padWidth, width - padWidth]);
+
+  const xAxis = d3.axisBottom(xScale).tickFormat((y) => y);
+
+  // Plot x-axis
+  svg
+    .append("g")
+    .call(xAxis)
+    .attr("id", "x-axis")
+    .attr("transform", `translate(0, ${height - padHeight})`);
+
+  // x-axis label
+  svg
+    .append("text")
+    .text("Year")
+    .attr("id", "x-label")
+    .attr("class", "axis-label")
+    .attr("x", "85%")
+    .attr("y", "97%");
 }
