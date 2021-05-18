f28c3a52d8081fd8d434168ba087bb6076d5628c
Jesse Devaney <22608765+JesseSDevaney@users.noreply.github.com>
2021-05-18 14:09:21 -0400

    Create y-axis and y-axis label
        - Convert month strings to numbers using the dataset
        - Create linear scale for y-axis
        - Plot y-axis
        - Plot y-label

--- src/app/app.js
+++ src/app/app.js
@@ -21,7 +21,7 @@ export default function createHeatMap(data) {
     .domain([d3.min(years) - 0.5, d3.max(years) + 0.5])
     .range([padWidth, width - padWidth]);
 
-  const xAxis = d3.axisBottom(xScale).tickFormat((y) => y);
+  const xAxis = d3.axisBottom(xScale).tickFormat((year) => year);
 
   // Plot x-axis
   svg
@@ -38,4 +38,32 @@ export default function createHeatMap(data) {
     .attr("class", "axis-label")
     .attr("x", "85%")
     .attr("y", "97%");
+
+  // y-axis
+  const months = data["monthlyVariance"].map(({ month }) =>
+    parseInt(month, 10)
+  );
+  const yScale = d3
+    .scaleLinear()
+    .domain([d3.min(months) - 0.5, d3.max(months) + 0.5])
+    .range([height - padHeight, padHeight]);
+
+  const yAxis = d3.axisLeft(yScale);
+
+  // Plot y-axis
+  svg
+    .append("g")
+    .call(yAxis)
+    .attr("id", "y-axis")
+    .attr("transform", `translate(${padWidth}, 0)`);
+
+  // y-axis label
+  svg
+    .append("text")
+    .text("Month")
+    .attr("id", "y-label")
+    .attr("class", "axis-label")
+    .attr("transform", "rotate(-90)")
+    .attr("x", "-18%")
+    .attr("y", "6%");
 }
