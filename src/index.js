3df5432a162dff36fcbd35a3ea2223d05d468a88
Jesse Devaney <22608765+JesseSDevaney@users.noreply.github.com>
2021-05-18 13:30:26 -0400

    Fetch and handle data. Pass data to createHeatMap
    - Fetch data and parse JSON (index.js)
    - Pass parsed JSON to createHeatMap (index.js)

--- src/index.js
+++ src/index.js
@@ -2,7 +2,17 @@ import "./index.scss";
 import createHeatMap from "./app/app";
 
 function ready() {
-  createHeatMap();
+  fetch(
+    "https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/master/global-temperature.json"
+  )
+    .then((response) => response.json())
+    .then((data) => {
+      createHeatMap(data);
+    })
+    .catch((error) => {
+      alert("Error loading content");
+      console.log(error);
+    });
 }
 
 if (document.readyState === "loading") {
